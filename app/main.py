3923c5db47c65660dbc3a979a9959eab5b32a035
Noakai Aronesty <74675547+naronesty@users.noreply.github.com>
2021-11-03 23:12:14 -0400

    authentication

--- app/main.py
+++ app/main.py
@@ -7,10 +7,10 @@
 from flask import Flask, render_template, request, session, redirect, url_for
 
 from app import app
-from app.auth import auth_user, create_user
+from app.auth import auth_user, create_user, create_db
 
 @app.route("/", methods=['GET', 'POST'])
-def home():
+def disp_loginpage():
     """ Display login page if there is no username in session, else display the
        response with the session username passed in. """
 
@@ -19,63 +19,49 @@ def home():
         return render_template('response.html',username=session['username'])
     return render_template('login.html')
 
-# authentication of login
-@app.route("/login", methods=['GET','POST'])
+# authetication of login 
+@app.route("/auth", methods=['GET','POST'])
 def authenticate():
     """ Checks whether method is get, post. If get method, then redirect to
        loginpage. If post, then authenticate the username and password, rendering
        the error page if incorrect and the response.html if correct username/pass. """
 
     # Variables
+    method = request.method
     username = request.form.get('username')
     password = request.form.get('password')
 
     # Get vs Post
-    if request.method == 'GET':
-        return redirect(url_for('home'))
-
-    # If user is located in database, create session
-    if auth_user(username,password):
-        session['username'] = username
-        return render_template('response.html',username=session['username'])
+    if method == 'GET':
+        return redirect(url_for('disp_loginpage'))
+    return auth_user(username, password)
 
 @app.route("/register")
 def register():
-    # Displays register page
+    #displays register page
     return render_template('register.html')
 
-# Authentication of username and passwords given in register page from user
+# authetication of username and passwords given in register page from user
 @app.route("/rAuth", methods =['GET', 'POST'])
 def rAuthenticate():
     method = request.method
     username = request.form.get('username')
     password0 = request.form.get('password0')
     password1 = request.form.get('password1')
-
+    
     if method == 'GET':
         return redirect(url_for('register'))
-
+    
     if method == 'POST':
-        # If the 2 passwords given don't match, will display error saying so
+        # if the 2 passwords given don't match, will display error saying so
         if password0 != password1:
             return render_template('register.html', mismatch = True)
         else:
-            #return render_template('login.html')
             # creates user account b/c no fails (pass match, username not taken)
-            if create_user(username, password0):
-                return render_template('login.html')
-            # username is taken, account creation fails, display such error
+            if create_user(username, password0) == False:
+                return render_template('register.html', input='a')
             else:
-                return render_template('register.html', taken = True)
-            '''commented out b/c create_user does not work, gives error that
-                                    "SQLite objects created in a thread can only be used in that same thread"
-            '''
-
-
-@app.route("/create")
-def create():
-    return render_template('create.html')
-
+                return render_template('login.html', input='success ')
 
 @app.route("/logout")
 def logout():
@@ -85,6 +71,6 @@ def logout():
     try:
         session.pop('username')
     except KeyError:
-        return redirect(url_for('home'))
+        return redirect(url_for('disp_loginpage'))
     # Redirect to login page
-    return redirect(url_for('home'))
+    return redirect(url_for('disp_loginpage'))
