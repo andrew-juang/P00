7bdf312c89664f7d1c0cecec1b955c095ba7fb10
Noakai Aronesty <74675547+naronesty@users.noreply.github.com>
2021-11-03 23:12:34 -0400

    Authentication

--- app/auth.py
+++ app/auth.py
@@ -5,47 +5,68 @@
 """Authentication/Creation of Users"""
 
 import sqlite3
+from flask import Flask, render_template, request, session, redirect, url_for
 
-DB_FILE="discobandit.db"
+DB_FILE = "discobandit.db"
 
-def auth_user(username, password):
-    """ Validates a username + password when person logs in """
+def create_db():
 
     db = sqlite3.connect(DB_FILE)
     c = db.cursor()
 
     c.execute("CREATE TABLE IF NOT EXISTS users (usernames TEXT, passwords TEXT);")
+    c.execute("CREATE TABLE IF NOT EXISTS blogs (usernames TEXT, blognames TEXT, id INTEGER, content TEXT);")
+    db.close()
+
+def auth_user(username, password):
+    db = sqlite3.connect(DB_FILE)
+    c = db.cursor()
+    """ Validates a username + password when person logs in """
 
     c.execute("SELECT usernames FROM users")
-    if username in c.fetchall():
-        return True
-    return True
+    users = []
+    for a_tuple in c.fetchall():
+        users.append(a_tuple[0])
 
-    db.commit()
-    db.close()
+    if username in users:
+        c.execute("SELECT passwords FROM users WHERE usernames = '" + username + "'")
+        if c.fetchall()[0][0] == password:
+            session['username'] = username
+            return render_template('response.html', username=session['username'])
+        else:
+            return render_template('login.html', input='Password is ')
+    else:
+        return render_template('login.html', input='Username is ')
 
 
 def create_user(username, password):
+    db = sqlite3.connect(DB_FILE)
+    c = db.cursor()
     """ Adds user to database if right username and password are given when a
         person registers """
 
-    db = sqlite3.connect(DB_FILE)
-    c = db.cursor()
-
-    c.execute("CREATE TABLE IF NOT EXISTS users (usernames TEXT, passwords TEXT);")
 
     # add more conditionals here
-    print(username in c.fetchall())
     # username is taken already, returns fail to display error
-    # NEED TO FIX THIS CONDITIONAL
-    if username in c.fetchall():
+    c.execute("SELECT usernames FROM users")
+    users = []
+    for a_tuple in c.fetchall():
+        users.append(a_tuple[0])
+
+    if username in users:
         return False
     # username is not taken, creates account with given username and password
     else:
-        c.execute("INSERT INTO users(usernames,passwords) VALUES (?, ?)", (username,password))
-        c.execute("SELECT * FROM users")
-        print(c.fetchall())
+        c.execute("INSERT INTO users VALUES (?, ?);", (username, password))
         db.commit()
-        db.close()
-        return True
-    # c.execute("SELECT * FROM users")
+
+create_db()
+
+db = sqlite3.connect(DB_FILE)
+c = db.cursor()
+db.commit()
+c.execute("SELECT usernames FROM users")
+users = []
+for a_tuple in c.fetchall():
+    users.append(a_tuple[0])
+print(users)
